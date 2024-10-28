892e360ddcd7ba3181639ee0088ac7304e9bd000
00014719 <117642084+00014719@users.noreply.github.com>
2024-10-28 22:06:41 +0500

    mvc app

---
+++ MVC/Controllers/MovieController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly HttpClient client;
+
+        public MovieController()
+        {
+            var clientAddress = new Uri("http://ec2-44-202-20-186.compute-1.amazonaws.com");
+            this.client = new HttpClient();
+            this.client.BaseAddress = clientAddress;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var movies = new List<Movie>();
+            var response = await client.GetAsync("/api/movies");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                movies = JsonConvert.DeserializeObject<List<Movie>>(message);
+            }
+
+            return View(movies);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var movie = new Movie();
+            var response = await client.GetAsync($"/api/movies/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                movie = JsonConvert.DeserializeObject<Movie>(message);
+            }
+
+            return View(movie);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Movie());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                var serialized = JsonConvert.SerializeObject(movie);
+                var payload = new StringContent(serialized, Encoding.UTF8, "application/json");
+
+                var response = await client.PostAsync("/api/movies", payload);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var movie = new Movie();
+            var response = await client.GetAsync($"/api/movies/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                movie = JsonConvert.DeserializeObject<Movie>(message);
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id,  Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                var serialized = JsonConvert.SerializeObject(movie);
+                var payload = new StringContent(serialized, Encoding.UTF8, "application/json");
+
+                var response = await client.PutAsync($"/api/movies/{id}", payload);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var movie = new Movie();
+            var response = await client.GetAsync($"/api/movies/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var message = await response.Content.ReadAsStringAsync();
+                movie = JsonConvert.DeserializeObject<Movie>(message);
+            }
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, Movie movie)
+        {
+            var response = await client.DeleteAsync($"/api/movies/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(movie);
+        }
+    }
+}
---
+++ MVC/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required, MinLength(3)]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        [Required, Range(0, 5)]
+        public float Rating { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public string Director { get; set; } = string.Empty;
+    }
+}
